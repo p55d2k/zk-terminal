91b665413a438b4630dec998b6da3c9e601478b4
p55d2k <kai@xcidic.com>
2024-04-18 15:33:10 +0800

    init

---
+++ lib/commands.ts
@@ -0,0 +1,13 @@
+export const commands = [
+  "ls", // complete
+  "cd", // complete
+  "pwd", // complete
+  "cat",
+  "echo", // complete
+  "touch",
+  "mkdir",
+  "rm",
+  "mv",
+  "cp",
+  "clear", // complete
+];
---
+++ lib/handler.ts
@@ -0,0 +1,34 @@
+import { commands } from "./commands";
+import { changeDirectory, listDirectory } from "./data";
+
+export const handleLastCommand = (
+  command: string,
+  currentDir: string,
+  setCurrentDir: (dir: string) => void
+): string => {
+  const commandName = command.split(" ")[0];
+  if (!commands.includes(commandName)) {
+    return `error: command not found: ${command}`;
+  }
+
+  if (commandName === "pwd") {
+    return currentDir;
+  } else if (commandName === "echo") {
+    return command.slice(5);
+  } else if (commandName === "ls") {
+    return listDirectory(currentDir);
+  } else if (commandName === "cd") {
+    const errorMessage = changeDirectory(
+      command.split(" ")[1],
+      currentDir,
+      setCurrentDir
+    );
+    return errorMessage instanceof Error
+      ? "error: " + errorMessage.message
+      : "";
+  } else if (commandName === "clear") {
+    return "";
+  } else {
+    return `error: command not implemented yet: ${commandName}`;
+  }
+};
